4bf501d34e343acce8a5e0e965f3b3e85d39794d
Mekadrom <zadarimm@gmail.com>
2021-06-02 23:30:42 -0400

    added bound utility method

--- src/main/java/com/higgs/staged/StageUtils.java
+++ src/main/java/com/higgs/staged/StageUtils.java
@@ -6,4 +6,14 @@ public final class StageUtils {
     public static double dist(final double x1, final double y1, final double x2, final double y2) {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));// + Math.pow((z2 - z1), 2));
     }
+
+    public static double bound(double d, final double min, final double max) {
+        if (d < min) {
+            d = min;
+        }
+        if (d > max) {
+            d = max;
+        }
+        return d;
+    }
 }
